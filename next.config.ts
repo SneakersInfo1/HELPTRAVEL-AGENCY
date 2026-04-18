a4ef9709cfdac4fd8817a98a7e4b07b2730f6841
SneakersInfo1 <135023418+SneakersInfo1@users.noreply.github.com>
2026-04-19 01:07:12 +0200

    Bundle analyzer (pnpm analyze) (#11)
    Co-authored-by: Twoja Nazwa <twoj@email.com>

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import bundleAnalyzer from "@next/bundle-analyzer";
 
 const csp = [
   "default-src 'self'",
@@ -71,4 +72,8 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+export default withBundleAnalyzer(nextConfig);
